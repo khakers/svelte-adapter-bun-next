eb11009dd885df0d702ffa817ba2f59696699d88
TheOrdinaryWow <25114850+TheOrdinaryWow@users.noreply.github.com>
2025-04-08 00:37:55 +0800

    fix: unresolved import in `kit-server`

--- src/server/kit-server.ts
+++ src/server/kit-server.ts
@@ -1,8 +1,12 @@
 import type { SSRManifest, ServerInitOptions } from "@sveltejs/kit";
-import { Server as KitServer } from "@sveltejs/kit";
+import type { Server as ServerType } from "@sveltejs/kit";
+
+const SERVER_PATH = "server";
 
 export async function buildKitServer(manifest: SSRManifest, init_options?: Omit<ServerInitOptions, "env">) {
-  const server = new KitServer(manifest);
+  const { Server } = require(`./${SERVER_PATH}/index.js`) as { Server: new (manifest: SSRManifest) => ServerType };
+
+  const server = new Server(manifest);
   await server.init({ env: Bun.env as Record<string, string>, ...init_options });
 
   return server;
