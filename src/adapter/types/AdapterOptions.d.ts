426751a69a0a81d82fa23c12d1280152f7f20b1f
TheOrdinaryWow <25114850+TheOrdinaryWow@users.noreply.github.com>
2025-04-09 03:11:24 +0800

    refactor: remove unused options in `adapter`

--- src/adapter/types/AdapterOptions.d.ts
+++ src/adapter/types/AdapterOptions.d.ts
@@ -5,14 +5,4 @@ export declare type AdapterOptions = {
   out?: string;
   /** Whether and how to compress static assets (default: false) */
   precompress?: boolean | CompressOptions;
-  /** Prefix for environment variables (default: "") */
-  // envPrefix?: string;
-  /** Whether to build in development mode (default: false) */
-  development?: boolean;
-  /** Whether to use dynamic origin resolution (default: false) */
-  dynamic_origin?: boolean;
-  /** Depth to use for X-Forwarded-For headers (default: 1) */
-  xff_depth?: number;
-  /** Whether to serve static assets (default: true) */
-  assets?: boolean;
 };
