872a7f1c323ed5331c3a54c40513f2e8bf8fe948
Khakers <22665282+khakers@users.noreply.github.com>
2025-05-10 22:55:20 -0700

    Improve static file serving
    Fix immutable cache control headers, implement Etag, last-modified, and precompressed asset serving. Supports if-modified-since and if-none-match 304 responses

--- src/server/routes/static.ts
+++ src/server/routes/static.ts
@@ -1,11 +1,106 @@
-import { existsSync } from "node:fs";
+import {existsSync} from "node:fs";
 import path from "node:path";
-import type { RouteReturn, RouteReturns } from "../types/RouteReturns";
-import { getManifestFile } from "../utils";
+import type {RouteReturn, RouteReturns} from "../types/RouteReturns";
+import {getManifestFile} from "../utils";
+import type {SSRManifest} from "@sveltejs/kit";
+
+// TODO doesn't support additional filetypes that might be added to precompress
+const PRECOMPRESSED_CONTENT_TYPES: Record<string, string> = {
+  "html": "text/html",
+  "js": "application/javascript",
+  "json": "application/json",
+  "css": "text/css",
+  "svg": "image/svg+xml",
+  "xml": "application/xml",
+  "wasm": "application/wasm",
+}
+
+
+// Get the file from the path. returns a precompressed file if it exists and is accepted
+async function getFile(path: string, brotli: boolean, gzip: boolean) {
+  // check for precompressed files
+  // return a precompressed file if exists and is accepted
+  const brFile = Bun.file(path + ".br");
+  const gzFile = Bun.file(path + ".gz");
+  if (brotli && await brFile.exists()) {
+    return brFile;
+  } else if (gzip && await brFile.exists()) {
+    return gzFile;
+  } else {
+    return Bun.file(path);
+  }
+}
+
+async function serveAsset(folderPath: string, manifest: SSRManifest, req: Request) {
+  try {
+    const pathname = new URL(req.url).pathname;
+    const filepath = path.join(folderPath, pathname);
+
+    const acceptedEncoding = req.headers.get("accept-encoding") || "";
+
+    const brotli = acceptedEncoding.includes("br") || acceptedEncoding.includes("brotli");
+    const gzip = acceptedEncoding.includes("gzip");
+
+    const file = await getFile(filepath, brotli, gzip);
+
+    const eTag = `W/"${file.size}-${file.lastModified}"`;
+
+    if (req.headers.get('if-none-match') === eTag) {
+      return new Response(null, {
+        status: 304
+      });
+    }
+
+    if (req.headers.get('if-modified-since') !== null) {
+      // return 304 if the file has not been modified since the if-modified-since date
+      const ifModifiedSince = new Date(req.headers.get('if-modified-since') || 0);
+      if (Math.floor(ifModifiedSince.getTime() / 1000) >= file.lastModified) {
+        return new Response(null, {
+          status: 304
+        });
+      }
+    }
+
+    const headers = new Headers({
+      "ETag": eTag,
+      "last-modified": new Date(file.lastModified).toUTCString(),
+      "vary": "accept-encoding"
+    });
+
+    if (pathname.startsWith(`/${manifest.appPath}/immutable/`)) {
+      headers.set('cache-control', 'public,max-age=31536000,immutable');
+    }
+
+    if (file.name?.slice(-3) === ".br") {
+      headers.set("content-encoding", "br");
+      // use the file extension that occurs before the .br
+      headers.set("content-type", PRECOMPRESSED_CONTENT_TYPES[file.name?.slice(0, file.name.length - 3).split('.').pop() ?? ''] || "application/octet-stream");
+    } else if (file.name?.slice(-3) === ".gz") {
+      headers.set("content-encoding", 'gzip');
+      headers.set("content-type", PRECOMPRESSED_CONTENT_TYPES[file.name?.slice(0, file.name.length - 3).split('.').pop() ?? ''] || "application/octet-stream");
+    }
+
+    return new Response(file, {
+      headers,
+      status: 200,
+    });
+  } catch (e) {
+    console.error(e);
+
+    return Response.json(
+      {
+        code: 500,
+        message: "Oops! Error occurred while serving static content.",
+        error: (e as Error).message,
+      },
+      {status: 500},
+    );
+  }
+}
 
 export async function buildStaticRoute(folderPath: string, isClient = false): Promise<RouteReturns> {
   if (!existsSync(folderPath)) {
-    console.warn("assets not exist, skipping serving static assets");
+    console.warn("assets directory does not exist, skipping serving static assets");
     return false;
   }
 
@@ -16,85 +111,12 @@ export async function buildStaticRoute(folderPath: string, isClient = false): Pr
 
   const appHandler: RouteReturn = {
     route: `/${appDir}/*`,
-    handler: async (req) => {
-      try {
-        const pathname = new URL(req.url).pathname;
-        const filepath = path.join(folderPath, pathname);
-
-        const file = Bun.file(filepath);
-
-        if (!(await file.exists())) {
-          const body = JSON.stringify({
-            code: 404,
-            message: "File not found.",
-          });
-          return new Response(body, { status: 404 });
-        }
-
-        const headers = new Headers({
-          "Content-Type": file.type,
-        });
-
-        if (isClient && pathname.startsWith(`/${manifest.appDir}/immutable/`)) {
-          headers.set("cache-control", "public,max-age=31536000,immutable");
-        }
-
-        return new Response(await file.arrayBuffer(), {
-          headers,
-          status: 200,
-        });
-      } catch (e) {
-        console.error(e);
-
-        return Response.json(
-          {
-            code: 500,
-            message: "Oops! Error occurred while serving static content.",
-            error: (e as Error).message,
-          },
-          { status: 500 },
-        );
-      }
-    },
+    handler: serveAsset.bind(null, folderPath, manifest),
   };
 
   const assetsHandler: RouteReturn[] = Array.from(assets).map((v) => ({
     route: `/${v}`,
-    handler: async () => {
-      try {
-        const file = Bun.file(path.join(folderPath, v));
-
-        if (!(await file.exists())) {
-          return Response.json(
-            {
-              code: 404,
-              message: "Asset not found.",
-            },
-            { status: 404 },
-          );
-        }
-
-        const headers = new Headers({
-          "Content-Type": file.type,
-        });
-
-        return new Response(await file.arrayBuffer(), {
-          headers,
-          status: 200,
-        });
-      } catch (e) {
-        console.error(e);
-
-        return Response.json(
-          {
-            code: 500,
-            message: "Oops! Error occurred while serving static content.",
-            error: (e as Error).message,
-          },
-          { status: 500 },
-        );
-      }
-    },
+    handler: serveAsset.bind(null, folderPath, manifest),
   }));
 
   return [appHandler, ...assetsHandler];
@@ -110,7 +132,7 @@ export async function buildDisabledStaticRoute(): Promise<RouteReturns> {
       handler: async () => {
         return Response.json(
           {
-            message: "Static assets serving is disabled.",
+            message: "Static asset serving is disabled.",
           },
           {
             status: 501,
